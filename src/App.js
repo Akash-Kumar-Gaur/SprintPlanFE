66b12af25c2ef0273c7ae9ec62351357d4ff093e
AkashKumarGaur <akashk@mplgaming.com>
2021-06-03 11:47:17 +0530

    remove firebase config

--- src/App.js
+++ src/App.js
@@ -9,14 +9,8 @@ import axios from "axios";
 
 function App() {
   var firebaseConfig = {
-    apiKey: "AIzaSyBS1I98jAMMABrxEIiDnTZahVdoGNlSDjY",
-    authDomain: "sprintplan-66723.firebaseapp.com",
-    databaseURL: "https://sprintplan-66723-default-rtdb.firebaseio.com",
-    projectId: "sprintplan-66723",
-    storageBucket: "sprintplan-66723.appspot.com",
-    messagingSenderId: "769079674943",
-    appId: "1:769079674943:web:3210f552edde59a11ed08a",
-    measurementId: "G-WK9Z6DC62B",
+    // removed my config
+    // {..your firbase config}
   };
   // Initialize Firebase
   if (!firebase.apps.length) {
